d6f460caa22e4e26bb2c26a5bb810b927604cb41
Tim <aquamaingi@gmail.com>
2023-01-23 11:21:55 +0300

    Webhooks Please

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! Its Me Timmy!!! Hope the webhook works";
+    private static final String MESSAGE = "Hello World! Its Me Timmy!!! Hope the webhook works Please";
 
     public App() {}
 
