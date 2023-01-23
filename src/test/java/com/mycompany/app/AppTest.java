24e11c603edfb04b3f35c9472a81be3d5d96a100
Tim <aquamaingi@gmail.com>
2023-01-23 13:12:22 +0300

    webhook trial

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World! Its Me Timmy!!! Hope the webhook works Please", app.getMessage());
+        assertEquals("Hello World! Its Me Timmy!!! Hope the webhook works", app.getMessage());
     }
 }
