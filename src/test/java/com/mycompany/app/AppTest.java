c61f31fbd020b60eec38539f205c42db659f81d9
Tim <aquamaingi@gmail.com>
2023-01-23 10:49:59 +0300

    FixedTest Suite

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World! Its me Timmy!!!", app.getMessage());
+        assertEquals("Hello World! Its Me Timmy!!!", app.getMessage());
     }
 }
